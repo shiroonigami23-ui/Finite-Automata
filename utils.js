fb7102d5b84053b6cdc674edefe2bcd51acf4e0c
shiroonigami23-ui <shiroonigami23@gmail.com>
2025-10-19 22:24:29 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -20,3 +20,27 @@ export function setValidationMessage(message, type) {
     validationLine.classList.add(type, 'show');
     setTimeout(() => validationLine.classList.remove('show'), 5000);
 }
+
+/**
+ * Adds a descriptive message to the step log.
+ * @param {string} message The HTML message to log.
+ * @param {string} icon The name of the Lucide icon to use.
+ */
+export function addLog(message, icon) {
+    const log = document.getElementById('stepLog');
+    if (!log) return;
+
+    const logEntry = document.createElement('div');
+    
+    logEntry.innerHTML = `<i data-lucide="${icon}"></i> <div>${message}</div>`;
+    
+    if (log.firstChild) {
+        log.insertBefore(logEntry, log.firstChild);
+    } else {
+        log.appendChild(logEntry);
+    }
+
+    if (typeof lucide !== 'undefined') {
+        lucide.createIcons({ nodes: [logEntry] });
+    }
+}
